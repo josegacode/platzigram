868c131c87276885a6367c27827bbf7f7666824d
Jose Garcia <josegar.dev@gmail.com>
2021-03-05 19:13:44 -0600

    Post model created, also implemented profile pictures upload in django admin

--- platzigram/settings.py
+++ platzigram/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -131,3 +132,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = "/static/"
+
+# Defines the path for images files taking the current
+# OS context path as a base dir, then join it with
+# the current project path
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+
+# URL in web browser for media files
+MEDIA_URL = "/media/"
--- platzigram/urls.py
+++ platzigram/urls.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
 from django.urls import path
+
+# Settings project file import
+from django.conf import settings
+from django.conf.urls.static import static
+
 from platzigram import views as localviews
 from posts import views as postviews
 
@@ -9,4 +14,5 @@ urlpatterns = [
     path("sorted-numbers/", localviews.sorted_numbers),
     path("welcome/<int:age>/<str:name>/", localviews.welcome),
     path("posts/", postviews.list_posts),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# Adds the media static path to the urlpatterns
